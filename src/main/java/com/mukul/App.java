60aabba854c7ded3cddaeaf2023340101ceed259
Mukul Tyagi <mukultyagi386@gmail.com>
2023-05-19 14:08:08 +0530

    Add Flatlaf

--- src/main/java/com/mukul/App.java
+++ src/main/java/com/mukul/App.java
@@ -1,11 +1,13 @@
 package com.mukul;
 
+import com.formdev.flatlaf.FlatLightLaf;
 import com.mukul.controller.AuthController;
 import com.mukul.model.AuthModel;
 import com.mukul.view.AuthView;
 
 public class App {
     public static void main(String[] args) {
+        FlatLightLaf.setup();
         AuthModel model = new AuthModel();
         AuthView view = new AuthView();
         new AuthController(model, view).showView();
